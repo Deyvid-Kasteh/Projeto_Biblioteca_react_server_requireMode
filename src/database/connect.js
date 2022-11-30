76db882ebd433d6eb5fab22a24d655f1d81f658e
Deyvid Kasteh <kasteh21@gmail.com>
2022-11-30 18:42:08 -0300

    resolvido conexao com atlas

--- src/database/connect.js
+++ src/database/connect.js
@@ -1,17 +1,19 @@
-const mongoose = require('mongoose');
-const connectToDatabase = async () => {
-    await mongoose.connect(
-        `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@biblio.bvlodsu.mongodb.net/?retryWrites=true&w=majority`,
-      (error) => {
-        if (error) {
-          return console.log(
-            "Ocorreu um erro ao se conectar ao banco de dados",
-            error
-          );
-        }
-        return console.log("Conexão estabelecida com sucesso!");
-      }
-    );
-}
+const mongoose = require("mongoose");
 
-module.exports = connectToDatabase
+const connectToDatabase = async () => {
+  await mongoose.connect(
+    `mongodb://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@ac-rba8asa-shard-00-00.mvy9ihr.mongodb.net:27017,ac-rba8asa-shard-00-01.mvy9ihr.mongodb.net:27017,ac-rba8asa-shard-00-02.mvy9ihr.mongodb.net:27017/?ssl=true&replicaSet=atlas-1l0pjz-shard-0&authSource=admin&retryWrites=true&w=majority`,
+    (error) => {
+      if (error) {
+        return console.log(
+          "Ocorreu um erro ao se conectar com o banco de dados: ",
+          error
+        );
+      }
+
+      return console.log("Conexão ao banco de dados realizada com sucesso!");
+    }
+  );
+};
+
+module.exports = connectToDatabase;
