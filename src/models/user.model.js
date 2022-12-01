91a4f5c33e4ad844b3b0ba0ae8e9812f4065ff77
Deyvid Kasteh <kasteh21@gmail.com>
2022-11-30 23:44:32 -0300

    add timestamps

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,24 +1,33 @@
 const mongoose = require('mongoose')
-const userSchema = new mongoose.Schema({
-    firstName: {
-        type: String,
-        required: true,
-    },
-    lastName: {
-        type: String,
-        required: true,
-    },
-    email: {
-        type: String,
-        required: true,
-    },
+const userSchema = new mongoose.Schema(
+    {
 
-    password: {
-        type: String,
-        required: true,
-        minlength: 7,
+        firstName: {
+            type: String,
+            required: true,
+        },
+
+        lastName: {
+            type: String,
+            required: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            index: { unique: true }
+        },
+        password: {
+            type: String,
+            required: true,
+            minlength: 7,
+        },
     },
-});
+    {
+        timestamps: true
+    }
+
+
+);
 
 const UserModel = mongoose.model("user", userSchema);
 
